4e3cb21d3c7472f311d4dd79761e92a4d8ef4462
Yarong Wang <wangyarong1266@gmail.com>
2023-10-21 17:07:28 -0700

    get profile and print info in console

--- StockApp/AddStockViewController.swift
+++ StockApp/AddStockViewController.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class AddStockViewController: UIViewController {
 
+    @IBOutlet weak var txtStock: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addStockAction(_ sender: Any) {
+        guard let stockSymbol = txtStock.text else { return }
+        
+        let url = "\(baseUrl)profile/\(stockSymbol.uppercased())?apikey=\(apiKey)"
+        
+        AF.request(url).responseJSON { response in
+            if (response.error != nil) {
+                print(response.error?.localizedDescription)
+                return
+            }
+            
+            guard let rawData = response.data else { return }
+            guard let jsonArray = JSON(rawData).array else { return }
+            guard let stock = jsonArray.first else { return }
+            
+            let symbol = stock["symbol"].stringValue
+            let price = stock["price"].floatValue
+            let companyName = stock["companyName"].stringValue
+            let description = stock["description"].stringValue
+            
+            print(symbol)
+            print(price)
+            print(companyName)
+            print(description)
+        }
     }
-    */
-
+    
 }
--- StockApp/Utils/Constants.swift
+++ StockApp/Utils/Constants.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+let apiKey = "1de789503732b50fbc3d7737b59a79b4"
+
+let baseUrl = "https://financialmodelingprep.com/api/v3/"
